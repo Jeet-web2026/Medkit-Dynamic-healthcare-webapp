051ad152ecb028b51a0800bebbde562675183ef3
Jit nath <150388284+Jeet-web2026@users.noreply.github.com>
2025-01-02 00:38:38 +0530

    Add files via upload

---
+++ medkit/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#navbarSupportedContentlast li').addClass('nav-item me-4');
+})
+
+$(document).ready(function(){
+    $('#navbarSupportedContentlast li a').addClass('text-black link-underline-light fw-bold');
+})
---
+++ medkit/page.php
@@ -0,0 +1,8 @@
+<?php
+get_header();
+get_header('link');
+?>
+    <h1 style="margin-top: 20vh;"><?php echo the_title()?></h1>
+<?php
+get_footer();
+?>
