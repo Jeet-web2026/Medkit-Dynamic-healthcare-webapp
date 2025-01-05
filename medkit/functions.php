2cb90dc0025ea7ca29258702e51edc45c77583e6
Jit nath <150388284+Jeet-web2026@users.noreply.github.com>
2025-01-05 23:51:22 +0530

    Add files via upload

---
+++ medkit/functions.php
@@ -0,0 +1,9 @@
+<?php
+register_nav_menus(
+    array('Primary-menu' => 'Top Menu')
+);
+
+add_theme_support('post-thumbnails');
+add_theme_support('custom-header');
+add_theme_support('custom-background');
+?>
