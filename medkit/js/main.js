31b0fc2eb259e951684bf1e567aacafe0a4fcad9
Jit nath <150388284+Jeet-web2026@users.noreply.github.com>
2025-01-05 23:45:30 +0530

    Delete medkit directory

--- medkit/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('#navbarSupportedContentlast li').addClass('nav-item me-4');
-})
-
-$(document).ready(function(){
-    $('#navbarSupportedContentlast li a').addClass('text-black link-underline-light fw-bold');
-})
--- medkit/page.php
+++
@@ -1,8 +0,0 @@
-<?php
-get_header();
-get_header('link');
-?>
-    <h1 style="margin-top: 20vh;"><?php echo the_title()?></h1>
-<?php
-get_footer();
-?>
