117fdd8753ee73e1b90456e2fd0438e6b79ebbe6
Jit nath <150388284+Jeet-web2026@users.noreply.github.com>
2025-01-05 23:50:44 +0530

    Delete medkit directory

--- medkit/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-register_nav_menus(
-    array('Primary-menu' => 'Top Menu')
-);
-
-add_theme_support('post-thumbnails');
-add_theme_support('custom-header');
-add_theme_support('custom-background');
-?>
